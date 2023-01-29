668f1da0c37a56a56a0fa741852b9271b5c97de2
Muhammad Nasir <muhnasir@students.uit.edu>
2023-01-30 00:53:46 +0500

    Updated sockets

--- src/socket.js
+++ src/socket.js
@@ -1,37 +1,23 @@
 import { io } from "socket.io-client";
 
-// const serverURl = "http://172.104.174.187:3054";
-const serverURl = "http://localhost:3054";
+const serverURl = "http://172.104.174.187:4064";
+// const serverURl = "http://localhost:3054";
 const socket = io(serverURl);
-
 
 //Event emitters.
 
 export const emitData = (data) => {
-    console.log('data-logs event triggered!')
-  socket.emit("data-logs", data);
-};
-
-export const emitAnalysis = (data) => {
-  console.log('analysis event triggered')
-socket.emit("analysis", data);
+    console.log('linux-logs event triggered!')
+  socket.emit("linux-logs", data);
 };
 
 //Event listeners.
 
-export const listenerData = (lines) => {
-  socket.on("data-logs",(d) => {
-    const {a, b} = d;
+  socket.on("linux-logs",(d) => {
+    // const {a, b} = d;
     console.log('listened data-logs event');
-    lines(b)
-    const myDiv = document.getElementById("data").innerHTML
-    document.getElementById("data").innerHTML = myDiv + a;
+    // lines(b)
+    // const myDiv = document.getElementById("data").innerHTML
+    // document.getElementById("data").innerHTML = myDiv + a;
   })
-}
 
-
-
-  socket.on("analysis",(d) => {
-    console.log('listened analysis event')
-    console.log('Analysis: ', d);
-  })
